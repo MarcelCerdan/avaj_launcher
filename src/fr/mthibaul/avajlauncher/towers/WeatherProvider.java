c6ea34fe6ac4c7b8e9f18114fc9900cf978b6f22
mthibaul <maxime.thibault21490@gmail.com>
2024-10-24 16:24:42 +0200

    Created all classes

--- src/fr/mthibaul/avajlauncher/towers/WeatherProvider.java
+++ src/fr/mthibaul/avajlauncher/towers/WeatherProvider.java
@@ -1,8 +1,16 @@
 package fr.mthibaul.avajlauncher.towers;
 
+import fr.mthibaul.avajlauncher.Coordinates;
+
 public class WeatherProvider {
 
-	private String[] weather;
+	private String[][][] weather;
 
-	
+	private WeatherProvider() {
+
+	}
+
+	public String getCurrentWeather(Coordinates p_coordinates) {
+		return weather[p_coordinates.getLongitude()][p_coordinates.getLatitude()][p_coordinates.getHeight()];
+	}
 }
